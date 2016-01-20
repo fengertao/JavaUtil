fdb84f0bd5b453b5f5f7d1742600dc9edd5b1f4c
Charlie Feng <cfeng@vmware.com>
2016-01-20 15:22:47 +0800

    Add GSON demo

---
+++ demo/src/main/java/charlie/feng/demo/gson/JsonDemo.java
@@ -0,0 +1,27 @@
+package charlie.feng.demo.gson;
+
+import com.google.gson.*;
+
+public class JsonDemo {
+
+	// run with -enableassertions
+	public static void main(String[] args) {
+		parseString();
+	}
+
+	public static void parseString() {
+		String jsonString = "{ \"input\" : {\"inputKey1\" : \"inputValue1\"}, \"output\" : {outputKey1: \"outputValue1\"}, \"array1\" : [{\"eleKey1\" : \"eleValue1\"}]}";
+		JsonElement jelement = new JsonParser().parse(jsonString);
+		JsonObject jobject = jelement.getAsJsonObject();
+		JsonObject inputJobject = jobject.getAsJsonObject("input");
+		assert inputJobject.toString().equals("{\"inputKey1\":\"inputValue1\"}");
+		JsonObject outputobject = jobject.getAsJsonObject("output");
+		// Notice that the outputKey1 is double quoted
+		assert outputobject.toString().equals("{\"outputKey1\":\"outputValue1\"}");
+		JsonArray jarray = jobject.getAsJsonArray("array1");
+		jobject = jarray.get(0).getAsJsonObject();
+		assert jobject.toString().equals("{\"eleKey1\":\"eleValue1\"}");
+
+	}
+
+}
