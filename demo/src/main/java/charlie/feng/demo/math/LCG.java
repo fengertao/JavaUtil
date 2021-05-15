86b8b6be01a22dbb769f1307fa872da7f9f9b31f
Charlie Feng <fengertao@outlook.com>
2021-05-15 15:19:16 +0800

    refactory LCG

--- demo/src/main/java/charlie/feng/demo/math/LCG.java
+++ demo/src/main/java/charlie/feng/demo/math/LCG.java
@@ -8,17 +8,15 @@ import com.google.common.math.IntMath;
 public class LCG {
     public static void main(String[] args) {
         int[] a = new int[]{1,2,4,8,5,10,20,40,25,50,100,200,101,202,404,808,505,1010,2020,4040,2525,5050,10100,20200};
-        int[] b = new int[]{1,2,4,8,5,10,20,40,25,50,100,200,101,202,404,808,505,1010,2020,4040,2525,5050,10100,20200};
-        int[] c = new int[]{1,2,4,8,5,10,20,40,25,50,100,200,101,202,404,808,505,1010,2020,4040,2525,5050,10100,20200};
         int result = 0;
 
         for (int i = 0; i < a.length; i++) {
-            for (int j = i; j < b.length; j++) {
-                for (int k = j; k < c.length; k++) {
-                    int lcg1 = a[i] * b[j] / IntMath.gcd(a[i], b[j]);
-                    int lcg = lcg1 * c[k] / IntMath.gcd(lcg1, c[k]);
+            for (int j = i; j < a.length; j++) {
+                for (int k = j; k < a.length; k++) {
+                    int lcg1 = a[i] * a[j] / IntMath.gcd(a[i], a[j]);
+                    int lcg = lcg1 * a[k] / IntMath.gcd(lcg1, a[k]);
                     if (lcg == 20200) {
-                        System.out.println(a[i] + " " + b[j] + " " + c[k]);
+                        System.out.println(a[i] + " " + a[j] + " " + a[k]);
                         result++;
                     }
                 }
